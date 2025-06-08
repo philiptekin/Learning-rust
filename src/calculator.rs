31b0456c536308c9e2aafdb5f12f35412d9226f4
Philip Tekin <philledaman@gmail.com>
2025-06-08 23:59:46 +0200

    Refactor calculator class to keep value

--- src/calculator.rs
+++ src/calculator.rs
@@ -1,16 +1,38 @@
-pub fn addition(a: i32, b: i32) -> i32 {
-    a + b
+pub struct Calculator {
+    current_value: i32,
 }
-pub fn subtraction(a: i32, b: i32) -> i32 {
-    a - b
-}
-pub fn multiplication(a: i32, b: i32) -> i32 {
-    a * b
-}
-pub fn division(a: i32, b: i32) -> Result<i32, String> {
-    if b == 0 {
-        Err("Cannot divide by zero".to_string())
-    } else {
-        Ok(a / b)
+
+impl Calculator {
+    pub fn new() -> Self {
+        Calculator { current_value: 0 }
+    }
+
+    pub fn get_current_value(&self) -> i32 {
+        self.current_value
+    }
+
+    pub fn reset(&mut self) {
+        self.current_value = 0;
+    }
+
+    pub fn add(&mut self, value: i32) {
+        self.current_value += value;
+    }
+
+    pub fn subtract(&mut self, value: i32) {
+        self.current_value -= value;
+    }
+
+    pub fn multiply(&mut self, value: i32) {
+        self.current_value *= value;
+    }
+
+    pub fn divide(&mut self, value: i32) -> Result<(), String> {
+        if value == 0 {
+            Err("Cannot divide by zero".to_string())
+        } else {
+            self.current_value /= value;
+            Ok(())
+        }
     }
 }
--- src/gui.rs
+++ src/gui.rs
@@ -1,5 +1,7 @@
-use crate::calculator;
+use crate::calculator::Calculator;
 use fltk::{app, button::Button, input::Input, prelude::*, window::Window};
+use std::cell::RefCell;
+use std::rc::Rc;
 
 static WINDOW_WIDTH: i32 = 300;
 static WINDOW_HEIGHT: i32 = 300;
@@ -7,10 +9,22 @@ static WINDOW_HEIGHT: i32 = 300;
 static OPERATOR_WIDTH: i32 = 40;
 static OPERATOR_HEIGHT: i32 = 20;
 
-fn initialize_gui() {
-    let button = setup_button(160, 200, OPERATOR_WIDTH, OPERATOR_HEIGHT, "+");
+static INPUT_WIDTH: i32 = 200;
+static INPUT_HEIGHT: i32 = 30;
 
-    let input = setup_input();
+fn initialize_gui() {
+    let calculator = Rc::new(RefCell::new(Calculator::new()));
+
+    let button = setup_button(
+        160,
+        200,
+        OPERATOR_WIDTH,
+        OPERATOR_HEIGHT,
+        "+",
+        calculator.clone(),
+    );
+
+    let input = setup_input(100, 20, INPUT_WIDTH, INPUT_HEIGHT);
 
     let mut window = Window::new(100, 100, WINDOW_WIDTH, WINDOW_HEIGHT, "Hello from rust");
     window.add(&button);
@@ -19,17 +33,27 @@ fn initialize_gui() {
     window.show();
 }
 
-fn setup_button(x: i32, y: i32, width: i32, height: i32, text: &str) -> Button {
+fn setup_button(
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+    text: &str,
+    calculator: Rc<RefCell<Calculator>>,
+) -> Button {
     let mut button = Button::new(x, y, width, height, text);
     button.set_callback(move |_| {
-        let result = calculator::addition(2, 3);
-        println!("Result of addition: {}", result);
+        calculator.borrow_mut().add(3);
+        println!(
+            "Result of addition: {}",
+            calculator.borrow_mut().get_current_value()
+        );
     });
     button
 }
 
-fn setup_input() -> Input {
-    let input = Input::new(100, 20, 200, 30, "");
+fn setup_input(x: i32, y: i32, width: i32, height: i32) -> Input {
+    let input = Input::new(x, y, width, height, "");
     input
 }
 
